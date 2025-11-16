9c1d0c2f077302eb179bc82afe1bb4abc0dce438
Jing Xuan Goh <gohjx8808@gmail.com>
2025-11-16 11:15:49 +0800

    added db migration info modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,6 +11,19 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 import * as SQLite from "expo-sqlite";
 import { drizzle } from "drizzle-orm/expo-sqlite";
+import { View } from "react-native";
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
+import migrations from "../drizzle/migrations";
+import { useEffect, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogBackdrop,
+  AlertDialogBody,
+  AlertDialogContent,
+} from "@/components/ui/alert-dialog";
+import { Text } from "@/components/ui/text";
+import { Spinner } from "@/components/ui/spinner";
+import { VStack } from "@/components/ui/vstack";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -21,6 +34,25 @@ export default function RootLayout() {
 
   const expo = SQLite.openDatabaseSync("db.db");
   const db = drizzle(expo);
+
+  const { success, error } = useMigrations(db, migrations);
+  const [isMigrating, setIsMigrating] = useState(false);
+
+  useEffect(() => {
+    if (!success && !error) {
+      setIsMigrating(true);
+    } else {
+      setIsMigrating(false);
+    }
+  }, [error, success]);
+
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
 
   return (
     <GluestackUIProvider mode="dark">
@@ -33,6 +65,19 @@ export default function RootLayout() {
           />
         </Stack>
         <StatusBar style="auto" />
+        <AlertDialog isOpen={isMigrating} closeOnOverlayClick={false}>
+          <AlertDialogBackdrop />
+          <AlertDialogContent>
+            <AlertDialogBody>
+              <VStack space="lg">
+                <Spinner />
+                <Text className="text-center">
+                  Please wait for a moment. Database migration is in progress.
+                </Text>
+              </VStack>
+            </AlertDialogBody>
+          </AlertDialogContent>
+        </AlertDialog>
       </ThemeProvider>
     </GluestackUIProvider>
   );

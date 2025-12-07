14a023b6f08ed0791dba9b18c1f65cfebc8ddf30
Jing Xuan Goh <gohjx8808@gmail.com>
2025-12-07 19:23:31 +0800

    display ingredient card

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from "@/components/ui/text";
 import { itemsTable } from "@/db/schema";
-import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import * as SQLite from "expo-sqlite";
 import { db } from "@/db/utils";
+import { Card } from "@/components/ui/card";
+import { Image } from "@/components/ui/image";
+import { Heading } from "@/components/ui/heading";
+import { View } from "react-native";
 
 const Index = () => {
   const [items, setItems] = useState<(typeof itemsTable.$inferSelect)[] | null>(
@@ -13,24 +15,67 @@ const Index = () => {
 
   useEffect(() => {
     const testInsert = async () => {
-      await db
-        .insert(itemsTable)
-        .values([
-          {
-            name: "testing",
-            quantity: 30,
-          },
-        ])
-        .onConflictDoNothing({ target: itemsTable.name });
-      const items = await db.select().from(itemsTable);
-      console.log(items);
+      const testData = {
+        name: "testing",
+        quantity: 30,
+        expiryDate: new Date("2025-12-12"),
+        testImgUri:
+          "https://www.lovefoodhatewaste.com/sites/default/files/styles/open_graph_image/public/2022-06/Carrots.jpg.webp?itok=aBgglla9",
+      };
+      await db.insert(itemsTable).values([testData]).onConflictDoUpdate({
+        target: itemsTable.name,
+        set: testData,
+      });
+      const itemsFromDB = await db.select().from(itemsTable);
+      setItems(itemsFromDB);
     };
     testInsert();
   }, []);
 
+  const expiryClass = (itemExpiryDate: Date | null) => {
+    if (itemExpiryDate) {
+      const daysBeforeExpiryInDays = Math.ceil(
+        (+itemExpiryDate - +new Date()) / (1000 * 60 * 60 * 24)
+      );
+
+      if (daysBeforeExpiryInDays > 7) {
+        return "text-green-600";
+      } else if (daysBeforeExpiryInDays > 3) {
+        return "text-yellow-600";
+      }
+      return "text-red-600";
+    }
+  };
+
   return (
     <SafeAreaView className="mx-10">
-      <Text>asdasd</Text>
+      {items?.map((item) => (
+        <Card className="p-5 rounded-lg m-3" key={item.id}>
+          <View className="items-center">
+            <Image
+              source={{
+                uri:
+                  item.imageUri ||
+                  "https://thumbs.dreamstime.com/b/no-image-icon-vector-available-picture-symbol-isolated-white-background-suitable-user-interface-element-205805243.jpg",
+              }}
+              className="mb-6 h-[240px] w-full rounded-md"
+              alt="image"
+            />
+          </View>
+          <Heading size="lg" className="mb-4">
+            {item.name}
+          </Heading>
+          <Text size="lg" className="mb-2">
+            Quantity: {item.quantity}
+          </Text>
+          <Text size="lg" className="mb-2">
+            Expiry Date:{" "}
+            <Text className={expiryClass(item.expiryDate)}>
+              {item.expiryDate?.toLocaleDateString()}
+            </Text>
+          </Text>
+        </Card>
+      ))}
     </SafeAreaView>
   );
 };
